32c76b44075fead7c3f7b7e0a4510e6ad6688df3
jose-montenegro-romero <149119278+jose-montenegro-romero@users.noreply.github.com>
2024-08-26 20:30:59 -0500

    add bootstrap and cookies.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
+// Routes
+import { routes } from './app.routes';
+// Interceptors
+import { authInterceptor } from '@interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withViewTransitions(), withComponentInputBinding()),
+    provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
+    provideClientHydration()
+  ]
 };
